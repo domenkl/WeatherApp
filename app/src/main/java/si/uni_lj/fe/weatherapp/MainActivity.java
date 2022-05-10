74cb28f886b2b487b6f62c383a309d25d634170e
Strikahh <domen.klander5@gmail.com>
2022-05-10 13:01:58 +0200

    Remove geocode lookup, use SharedPreferences for saving data, add string for unsucessful lookup

--- app/src/main/java/si/uni_lj/fe/weatherapp/MainActivity.java
+++ app/src/main/java/si/uni_lj/fe/weatherapp/MainActivity.java
@@ -2,24 +2,36 @@ package si.uni_lj.fe.weatherapp;
 
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import si.uni_lj.fe.weatherapp.data.CurrentData;
+import si.uni_lj.fe.weatherapp.models.CurrentDataModel;
 import si.uni_lj.fe.weatherapp.util.CallbackFuture;
 import si.uni_lj.fe.weatherapp.util.OkHttpSingleton;
 import si.uni_lj.fe.weatherapp.util.UrlBuilder;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final OkHttpSingleton clientSingleton = OkHttpSingleton.getInstance();
+    private final OkHttpClient client = OkHttpSingleton.getInstance().getClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,34 +52,70 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void onSearch(View v) {
-        EditText searchBar = findViewById(R.id.search_bar);
-        if (searchBar.getText().toString().equals("")) {
+        String cityName = ((EditText) findViewById(R.id.search_bar)).getText().toString();
+        if (cityName.equals("")) {
             Toast.makeText(this, R.string.city_empty, Toast.LENGTH_SHORT).show();
-        } else {
-            Request request = new Request.Builder()
-                    .url(UrlBuilder.getGeocodeUrl(searchBar.getText().toString()))
-                    .build();
-            OkHttpClient client = clientSingleton.getClient();
-            try {
-                CallbackFuture future = new CallbackFuture();
-                client.newCall(request).enqueue(future);
-                Response response = future.get();
-
-                if (!response.isSuccessful()) {
-                    Toast.makeText(this, R.string.city_not_found, Toast.LENGTH_SHORT).show();
-                } else {
-                    Intent intent = new Intent(MainActivity.this, WeeklyActivity.class);
-                    intent.putExtra("cityName", searchBar.getText().toString());
-                    startActivity(intent);
-                }
-
-            } catch (Exception e) {
-                e.printStackTrace();
+            return;
+        }
+        try {
+            Response response = getDailyWeatherResponse(cityName);
+            if (!response.isSuccessful()) {
+                Toast.makeText(this, R.string.unsuccessful, Toast.LENGTH_SHORT).show();
+                return;
             }
+            if (response.body() != null) {
+                Gson gson = new Gson();
+                CurrentDataModel currentDataModel = gson.fromJson(response.body().string(), CurrentDataModel.class);
+                CurrentData currentData = new CurrentData(currentDataModel);
+
+                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                SharedPreferences.Editor editor = preferences.edit();
+                String json = gson.toJson(currentData);
+                editor.putString("currentData", json);
+                editor.putString("selectedCity", cityName);
+                editor.apply();
+                saveWeeklyDataToSharedPreferences(currentData.getLat(), currentData.getLon());
+
+                Intent intent = new Intent(MainActivity.this, WeeklyActivity.class);
+                startActivity(intent);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
     private void onUseLocation(View v) {
 
+    }
+
+    private Response getDailyWeatherResponse(String cityName) throws ExecutionException, InterruptedException {
+        Request request = new Request.Builder()
+                .url(UrlBuilder.getCurrentWeatherUrl(cityName))
+                .build();
+        CallbackFuture future = new CallbackFuture();
+        client.newCall(request).enqueue(future);
+        return future.get();
+    }
+
+    private void saveWeeklyDataToSharedPreferences(double lat, double lon) {
+        Request request = new Request.Builder()
+                .url(UrlBuilder.getOneCallWeatherUrl(lat, lon))
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.body() != null) {
+                    SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putString("oneCallData", response.body().string());
+                    editor.apply();
+                }
+            }
+        });
     }
 }
