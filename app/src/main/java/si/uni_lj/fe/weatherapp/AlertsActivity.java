c90bfd9c0ba04b20695c2a4d414f0379ceb21004
domenkl <domen.kl36@gmail.com>
2022-05-12 15:36:08 +0200

    Add alerts activity, layout

---
+++ app/src/main/java/si/uni_lj/fe/weatherapp/AlertsActivity.java
@@ -0,0 +1,15 @@
+package si.uni_lj.fe.weatherapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AlertsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
