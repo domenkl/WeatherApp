c6653e0d86ba26a50ef316b153107aac1cf32acf
domenkl <domen.kl36@gmail.com>
2022-05-28 13:33:42 +0200

    Add receiver to manifest, log received alerts

--- app/src/main/java/si/uni_lj/fe/weatherapp/services/AlertReceiver.java
+++ app/src/main/java/si/uni_lj/fe/weatherapp/services/AlertReceiver.java
@@ -3,12 +3,16 @@ package si.uni_lj.fe.weatherapp.services;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.widget.Toast;
+import android.util.Log;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 public class AlertReceiver extends BroadcastReceiver {
+
     @Override
     public void onReceive(Context context, Intent intent) {
-
-        Toast.makeText(context, "Hello wOrLd", Toast.LENGTH_SHORT).show();
+        Log.i("Context", context.toString());
+        Log.i("Alarm", "received" + " " + LocalDateTime.ofEpochSecond(System.currentTimeMillis() / 1000, 0, ZoneOffset.UTC));
     }
 }
