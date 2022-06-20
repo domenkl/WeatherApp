5d9e68367c2ed25e8c10652d0ab024433b68a335
domenkl <domen.kl36@gmail.com>
2022-06-20 11:00:10 +0200

    Add daily activity, layout, data

---
+++ app/src/main/java/si/uni_lj/fe/weatherapp/DailyActivity.java
@@ -0,0 +1,60 @@
+package si.uni_lj.fe.weatherapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import si.uni_lj.fe.weatherapp.data.DailyData;
+import si.uni_lj.fe.weatherapp.databinding.ActivityDailyBinding;
+import si.uni_lj.fe.weatherapp.models.DailyInfo;
+import si.uni_lj.fe.weatherapp.models.OneCallDataModel;
+
+public class DailyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+        int position = intent.getIntExtra("day", 0);
+        String city = intent.getStringExtra("city");
+        String country = intent.getStringExtra("country");
+
+        OneCallDataModel oneCallDataModel = getOneCallDataFromShared();
+        DailyInfo dailyInfo = oneCallDataModel.getDaily().get(position);
+        ActivityDailyBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_daily);
+        DailyData dailyData = new DailyData(dailyInfo, city, country);
+        binding.setDailyData(dailyData);
+        setImageResource(this, R.id.weather_icon, dailyData.getIcon());
+    }
+
+    private OneCallDataModel getOneCallDataFromShared() {
+        SharedPreferences preferences = getSharedPreferences("savedWeatherData", MODE_PRIVATE);
+        String oneCallData = preferences.getString("oneCallData", "");
+        return new Gson().fromJson(oneCallData, OneCallDataModel.class);
+    }
+
+    @SuppressWarnings("SameParameterValue")
+    private void setImageResource(Context context, int viewId, String imageName) {
+        try {
+            ImageView view = findViewById(viewId);
+            InputStream is = context.getAssets().open(String.format("weather/%s.png", imageName));
+            Drawable drawable = Drawable.createFromStream(is, null);
+            view.setImageDrawable(drawable);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ app/src/main/java/si/uni_lj/fe/weatherapp/data/DailyData.java
@@ -0,0 +1,118 @@
+package si.uni_lj.fe.weatherapp.data;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+import si.uni_lj.fe.weatherapp.models.DailyInfo;
+
+public class DailyData {
+
+    private final String city;
+    private final String country;
+    private final String day;
+    private final String icon;
+    private final String description;
+    private final int temperature;
+    private final int pressure;
+    private final int humidity;
+    private final String windSpeed;
+    private final int cloudiness;
+    private final String precipitation;
+    private final String uviIndex;
+    private final long sunrise;
+    private final long sunset;
+    private final String visibility;
+
+    public DailyData(DailyInfo data, String city, String country) {
+        this.city = city;
+        this.country = country;
+        this.sunrise = data.getSunrise();
+        this.sunset = data.getSunset();
+        this.day = setDate(data.getDt());
+        this.temperature = (int) Math.round(data.getTemperatures().getDay());
+        this.icon = data.getWeather().get(0).getIcon();
+        this.precipitation = (int) Math.round(data.getRain() * 100) + "";
+        this.description = setDescription(data.getWeather().get(0).getDescription());
+        this.cloudiness = data.getClouds();
+        this.windSpeed = (double) Math.round(data.getWindSpeed() * 36) / 10 + "km/h";
+        this.humidity = data.getHumidity();
+        this.uviIndex = data.getUvi() + "";
+        this.pressure = data.getPressure();
+        this.visibility = "10km";
+    }
+
+    private String setDescription(String description) {
+        return description.substring(0, 1).toUpperCase() + description.substring(1);
+    }
+
+    private String setDate(long dt) {
+        Locale.setDefault(new Locale("sl", "SI"));
+        Instant instant = Instant.ofEpochSecond(dt);
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("E d.M");
+        LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+        return dtf.format(localDateTime);
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public int getPressure() {
+        return pressure;
+    }
+
+    public int getHumidity() {
+        return humidity;
+    }
+
+    public String getWindSpeed() {
+        return windSpeed;
+    }
+
+    public int getCloudiness() {
+        return cloudiness;
+    }
+
+    public String getPrecipitation() {
+        return precipitation;
+    }
+
+    public String getUviIndex() {
+        return uviIndex;
+    }
+
+    public long getSunrise() {
+        return sunrise;
+    }
+
+    public long getSunset() {
+        return sunset;
+    }
+
+    public String getVisibility() {
+        return visibility;
+    }
+}
